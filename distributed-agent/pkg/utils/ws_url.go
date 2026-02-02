02a0537aa91f4f4e494024c21388b04957b79d02
Abhijit Roy <abhijitroy@usthaan.in>
2026-02-02 13:02:06 +0530

    Changed config

--- distributed-agent/pkg/utils/ws_url.go
+++ distributed-agent/pkg/utils/ws_url.go
@@ -7,6 +7,6 @@ import (
 
 func BuildWebSocketURL(baseURL, agentID string) string {
 	wsURL := strings.Replace(baseURL, "https", "wss", 1)
-	// wsURL = strings.Replace(wsURL, "http", "ws", 1) // local testing. uncomment in prod
+	wsURL = strings.Replace(wsURL, "http", "ws", 1) // local testing. uncomment in prod
 	return fmt.Sprintf("%s/ws?role=agent:%s", wsURL, agentID)
 }
